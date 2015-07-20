ccbb923899489d1964b58342f9193ddfd2a22051
Paul <pwittschen@gmail.com>
2015-07-20 18:51:36 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,49 @@
 $(function() {
+
+	$('#login').fadeIn('slow');
+	$('#username_input').focus();
+
+	/* ====================================== */
+	/*        Basic Chat functionality        */
 	var socket = io.connect();
+	var $chat_message = $('#chat_input_form'),
+		$chat_window  = $('#chat_window');
+	$('#check_username').on('submit', function(e) {
+		e.preventDefault();
+		socket.emit('client -- new user', $('#username_input').val(), function(data) {
+			if (data) {  // if username isn't taken...
+				$('#login').hide();
+				$('#chat_view').fadeIn('slow');
+			} else {
+				$('#login_error').html('Username already taken.');
+			}
+		});
+		$('#username_input').val('');
+	});
+	$chat_message.on('submit', function(e) {
+		e.preventDefault();
+		var $message = $('#message_input');
+		if ( $message.val() ) {
+			socket.emit('client -- send message', $message.val());
+			$message.val('');
+		}
+	});
+	socket.on('server -- new user', function(data) {
+		$chat_window.append('<div class="msg_status"><span class="chat_username">' + data + '</span> has entered the room</div>');
+	});
+	socket.on('server -- new message', function(data) {
+		$chat_window.append('<div class="msg"><span class="chat_username">' + data.username + '</span>: ' + data.message + '</div>');
+	});
+	socket.on('server -- user left', function(data) {
+		$chat_window.append('<div class="msg_status"><span class="chat_username">' + data + '</span> has left the room</div>');
+	});
+	socket.on('server -- update usernames', function(data) {
+		var html_str = '', i = 0;
+		for (; i < data.length; i++) {
+			html_str += '<div class="username_list_iten">' + data[i] + '</div>';
+		}
+		$('#username_list').html(html_str);
+	});
+
+
 });
